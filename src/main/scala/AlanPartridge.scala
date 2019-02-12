f69a6ad379e7c4c2ce214c39d41ada7e38ad1b63
Andrew.Brown <Andrew.Brown@digital.hmrc.gov.uk>
2019-02-12 15:29:47 +0000

    Working Kata

--- src/main/scala/AlanPartridge.scala
+++ src/main/scala/AlanPartridge.scala
@@ -1,23 +1,78 @@
-object AlanPartridge {
+object AlanPartridge extends App {
+//
+//  val relatedTerms = List("Partridge",
+//                          "PearTree",
+//                          "Chat",
+//                          "Dan",
+//                          "Toblerone",
+//                          "Lynn",
+//                          "AlphaPapa",
+//                          "Nomad")
+//
+//  val listOfWords = List("Too", "from", "blah", "Lynn", "Nomad")
+//
+//  def speak(words: List[String]) : Unit = {
+//    for(x <- listOfWords) {
+//      if (relatedTerms.contains(x)) {
+//        println("Mine's a pint!")
+//      }
+//    }
+//  }
+//  speak(relatedTerms)
+//}
 
-  val relatedTerms = List("Partridge",
-                          "PearTree",
-                          "Chat",
-                          "Dan",
-                          "Toblerone",
-                          "Lynn",
-                          "AlphaPapa",
-                          "Nomad")
 
-  val words = List("Too", "from", "blah", "Lynn", "Nomad")
+//  val relatedTerms = List("Partridge",
+//    "PearTree",
+//    "Chat",
+//    "Dan",
+//    "Toblerone",
+//    "Lynn",
+//    "AlphaPapa",
+//    "Nomad")
+//
+//  val listOfWords = List("Too", "from", "blah")
+//
+//  def numberOfMarks () : String = {
+//    var NumberOfMarks = exclamationMarkCounter(listOfWords)
+//    "!" * NumberOfMarks
+//  }
+//
+//  def exclamationMarkCounter (word : List[String]) : Int = {
+//      relatedTerms.count(term=>listOfWords.contains(term))
+//  }
+//
+//  if (exclamationMarkCounter(listOfWords) > 1){
+//    println (s"Mines a pint${numberOfMarks()}")
+//  }
+//  else println("Lynn, I've pierced my foot on a spike!")
+//
+//
+//}
 
-  speak(words)
+val relatedTerms = List("Partridge",
+  "PearTree",
+  "Chat",
+  "Dan",
+  "Toblerone",
+  "Lynn",
+  "AlphaPapa",
+  "Nomad")
 
-  def speak(words: List[String]) : String = ???
+val listOfWords = List("Too", "from", "blah","Lynn","Nomad","Dan","Chat")
 
-  // when you find a word return Mine's a Pint!
-  // for how many words you have found you return that many !
+def numberOfMarks (wordList : List[String]) : String = {
+  var NumberOfMarks = exclamationMarkCounter(wordList)
+  "!" * NumberOfMarks
+}
 
-  // when you don't find something return Lynn, I've pierced my foot on a spike!!
+def exclamationMarkCounter (word : List[String]) : Int = {
+  relatedTerms.count(term=>listOfWords.contains(term))
+}
+
+if (exclamationMarkCounter(listOfWords) > 1){
+  println (s"Mine's a pint${numberOfMarks(listOfWords)}")
+}
+else println("Lynn, I've pierced my foot on a spike!")
 
 }
