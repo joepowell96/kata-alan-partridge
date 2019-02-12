20cedd71983b0782c4dbb02be5a501f361c2d2de
Joe Powell <joe.powell@digital.hmrc.gov.uk>
2019-02-12 13:47:36 +0000

    Setup base structure

---
+++ src/main/scala/AlanPartridge.scala
@@ -0,0 +1,23 @@
+object AlanPartridge {
+
+  val relatedTerms = List("Partridge",
+                          "PearTree",
+                          "Chat",
+                          "Dan",
+                          "Toblerone",
+                          "Lynn",
+                          "AlphaPapa",
+                          "Nomad")
+
+  val words = List("Too", "from", "blah", "Lynn", "Nomad")
+
+  speak(words)
+
+  def speak(words: List[String]) : String = ???
+
+  // when you find a word return Mine's a Pint!
+  // for how many words you have found you return that many !
+
+  // when you don't find something return Lynn, I've pierced my foot on a spike!!
+
+}
